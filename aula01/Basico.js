a45fbb430d53c98a9ad5dbee285b4db5c3fedcf3
Priscila <priscilaribeirofce@gmail.com>
2026-03-19 22:20:14 -0300

    basico js

---
+++ aula01/Basico.js
@@ -0,0 +1,9 @@
+// Javascript é uma linguagem fracamente tipada 
+
+var variavelVar = "Olá, ";
+let variaveLet = 'tudo bem?';
+const variavelConst = 19;
+ano = 2026;
+
+
+console.log(variavelVar, variaveLet, "Hoje é dia ", variavelConst, " de março de ", ano, ".");
